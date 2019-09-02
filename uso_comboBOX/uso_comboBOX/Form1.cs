bf931de56e8db786716ffe20db3d05dd16dcabb9
DORIAN RAYGOZA <doriansamuelraygozaalvarado@gmail.com>
2019-09-02 09:56:16 -0500

    FUNCION AGREGAR

---
+++ uso_comboBOX/uso_comboBOX/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uso_comboBOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = comboBox1.SelectedIndex;
+            label1.Text = indice.ToString();
+            label2.Text = comboBox1.Items[indice].ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("pozole");
+            comboBox1.Items.Add("Tostadas de Pata");
+        }
+    }
+}
